bcfb999736228925a907e6392a3b9f5ff1c74fae
ayshathlubabaka <ayshathlubabaka@gmail.com>
2024-02-16 03:19:16 +0300

    routing edit

--- qatarfiesta/asgi.py
+++ qatarfiesta/asgi.py
@@ -1,13 +1,15 @@
 import os
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.channels_middleware import JWTWebsocketMiddleware
 from chat import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qatarfiesta.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http":django_asgi_app,
