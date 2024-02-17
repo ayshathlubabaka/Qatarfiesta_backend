cac56077b437c07bc1656fd5a919e4b9eab395a7
ayshathlubabaka <ayshathlubabaka@gmail.com>
2024-02-17 12:35:40 +0300

    whitenoise removed

--- qatarfiesta/settings.py
+++ qatarfiesta/settings.py
@@ -186,9 +186,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
